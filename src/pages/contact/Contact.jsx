4e3514d677ccfc30ba9dcf093b038bec45f3b079
Aulkan <capstone.graduation2023@gmail.com>
2023-10-17 09:36:20 +0800

    ds

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -80,7 +80,7 @@ const Contact = () => {
     const currentYear = new Date().getFullYear();
     const years = [];
 
-    for (let year = currentYear; year >= 1900; year--) {
+    for (let year = currentYear; year >= 2012; year--) {
       years.push(year);
     }
 
